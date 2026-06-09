4b838499ed94d2b5a1b6a6782fe376c2aca39902
icheong <i.cheong@gmail.com>
2026-06-09 16:56:56 +1000

    feat: add GA4 with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   site: 'https://utilifygrid.com',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: 'static',
+  integrations: [partytown()],
 });
